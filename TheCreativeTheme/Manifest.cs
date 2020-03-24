430c753c059f50e3f8f1bfd0e6f84bb5748eab0a
gang89liu <asd6224868@hotmail.com>
2020-03-25 02:40:04 +0800

    从0开始做theme, 学习了如何自定义菜单

---
+++ TheCreativeTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "The Creative Theme",
+    Author = "The Orchard Team",
+    Website = "https://orchardproject.net",
+    Version = "2.0.0",
+    Description = "A on page Bootstrap theme for creatives, small businesses, and other multipurpose use."
+)]
---
+++ TheCreativeTheme/Startup.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OrchardCore.Modules;
+using OrchardCore.ResourceManagement;
+
+namespace OrchardCore.Themes.TheCreativeTheme
+{
+    public class Startup : OrchardCore.Modules.StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IResourceManifestProvider, ResourceManifest>();
+        }
+    }
+}
